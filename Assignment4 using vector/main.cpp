bf1a334f0ad19b7797bdc02594043468edc8ced1
NaziaToma <44084527+NaziaToma@users.noreply.github.com>
2018-12-20 21:39:00 +0600

    Update main.cpp

--- Assignment4 using vector/main.cpp
+++ Assignment4 using vector/main.cpp
@@ -11,7 +11,7 @@ using namespace std;
 int main() {
 	int arr[] = { 14,33,27,10,35,19,48,44 };
 	SelectionSort number;
-    std::vector <int> first(std::begin(arr), std::end(arr));
+        std::vector <int> first(std::begin(arr), std::end(arr));
 	number.SelectionSorter(arr, 8);
 	number.printArray(arr, 8);
 	
